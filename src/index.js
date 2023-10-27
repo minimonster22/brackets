f85b838f691c907a004b6a02a19bb7db6f3d9321
minimonster22 <tamaramonstr@gmail.com>
2023-10-27 23:43:56 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const queue = [];
+
+    for (const char of str) {
+        const pair = bracketsConfig.find(config => config[0] === char || config[1] === char);
+
+        if (pair[0] === pair[1]) {
+            if (queue.length > 0 && queue[queue.length - 1] === char) {
+                queue.pop();
+            } else {
+                queue.push(char);
+            }
+        } else if (char === pair[0]) {
+            queue.push(char);
+        } else {
+            if (queue.length === 0 || queue.pop() !== pair[0]) {
+                return false;
+            }
+        }
+    }
+
+    return queue.length === 0;
 }
